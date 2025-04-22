5b4931e841784b8443acfe54d80643a2b4fa76b8
Rathapol.K <>
2025-04-22 11:42:40 +0700

    Add TimePicker component to form library for time selection with error handling and customization options

---
+++ src/lib/form/TimePicker.tsx
@@ -0,0 +1,48 @@
+import { TimePicker as MuiTimePicker } from '@mui/x-date-pickers/TimePicker'
+import { TimePickerProps as MuiTimePickerProps } from '@mui/x-date-pickers/TimePicker'
+import { useFieldContext } from './formHooks'
+import dayjs from 'dayjs'
+import { useMemo } from 'react'
+
+type Props = Omit<
+  MuiTimePickerProps,
+  'name' | 'value' | 'onChange' | 'defaultValue'
+> & {
+  required?: boolean
+  labelShrink?: boolean
+  size?: 'small' | 'medium'
+}
+export function TimePicker(props: Props) {
+  const field = useFieldContext<Date | string>()
+
+  const errorText = useMemo(() => {
+    if (field.state.meta.errors.length === 0) return null
+    return field.state.meta.errors.map(error => error.message).join(', ')
+  }, [field.state.meta.errors])
+
+  const { required, labelShrink, size, ...rest } = props
+  return (
+    <MuiTimePicker
+      {...rest}
+      name={field.name}
+      value={field.state.value ? dayjs(field.state.value) : null}
+      onChange={value => {
+        if (value) {
+          field.handleChange(value.toDate())
+        }
+      }}
+      slotProps={{
+        textField: {
+          required: required,
+          error: Boolean(errorText),
+          helperText: errorText,
+          InputLabelProps: { shrink: labelShrink },
+          size: size,
+          InputProps: {
+            notched: labelShrink,
+          },
+        },
+      }}
+    />
+  )
+}
