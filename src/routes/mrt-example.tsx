91493b7cd608c02186abd2754f7481c54084e00b
Rathapol.K <>
2025-05-15 17:00:37 +0700

    Remove unused import of useMemo in mrt-example.tsx

--- src/routes/mrt-example.tsx
+++ src/routes/mrt-example.tsx
@@ -1,8 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useMaterialRouterTable } from '../lib'
 import { MaterialReactTable } from 'material-react-table'
-import cloneDeep from 'clone-deep'
-import { useMemo } from 'react'
 
 const columns = [
   {
