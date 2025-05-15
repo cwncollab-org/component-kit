d725718a38c73c4d20c64974edb17ac73c1f8dc3
Rathapol.K <>
2025-05-15 11:15:37 +0700

    Update package version to 0.4.2 and enhance SubscribeButton component to include showIndicator prop for loading state.

--- src/lib/form/SubscribeButton.tsx
+++ src/lib/form/SubscribeButton.tsx
@@ -1,15 +1,21 @@
 import { Button, ButtonProps } from '@mui/material'
 import { useFormContext } from './formContext'
 
-export type SubscribeButtonProps = ButtonProps
+export type SubscribeButtonProps = ButtonProps & {
+  showIndicator?: boolean
+}
 
 export function SubscribeButton(props: SubscribeButtonProps) {
   const form = useFormContext()
-  const { children, disabled, ...rest } = props
+  const { children, disabled, showIndicator, ...rest } = props
   return (
     <form.Subscribe selector={state => state.isSubmitting}>
       {isSubmitting => (
-        <Button disabled={isSubmitting || disabled} {...rest}>
+        <Button
+          disabled={isSubmitting || disabled}
+          loading={showIndicator && isSubmitting}
+          {...rest}
+        >
           {children}
         </Button>
       )}
